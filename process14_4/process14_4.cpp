0ff48aa52aa3e0e911791b07dcfe70b6d7e5006b
IsPorCHen <fazy2019@gmail.com>
2025-02-20 19:33:37 +0500

    Добавьте файлы проекта.

---
+++ process14_4/process14_4.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <thread>
+#include <random>
+#include <Windows.h>
+#include <chrono>
+
+const LONG SIZE_ARR = 10;
+LONG arr[SIZE_ARR];     
+
+void modifyArray(LPVOID) {
+    srand(time(0));
+
+    for (int i = 0; i < SIZE_ARR; ++i) {
+        arr[i] = rand() % 301 - 150;
+    }
+}
+
+void replacePositiveNumbers(LPVOID) {
+    while (true) {
+        for (int i = 0; i < SIZE_ARR; ++i) {
+            if (arr[i] > 0) {
+                InterlockedExchange(&arr[i], 0);
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
+void printArray(LPVOID) {
+    while (true) {
+        for (int i = 0; i < SIZE_ARR; ++i) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
+int main() {
+    modifyArray(nullptr);
+
+    HANDLE hThread1 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)modifyArray, NULL, 0, NULL);
+    HANDLE hThread2 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)replacePositiveNumbers, NULL, 0, NULL);
+    HANDLE hThread3 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)printArray, NULL, 0, NULL);
+
+    WaitForSingleObject(hThread1, INFINITE);
+    WaitForSingleObject(hThread2, INFINITE);
+    WaitForSingleObject(hThread3, INFINITE);
+
+
+    CloseHandle(hThread1);
+    CloseHandle(hThread2);
+    CloseHandle(hThread3);
+
+    return 0;
+}
---
+++ process14_9/process14_9.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <thread>
+#include <random>
+#include <Windows.h>
+#include <chrono>
+
+const int ARRAY_SIZE = 10;
+LONG arr[ARRAY_SIZE];
+CRITICAL_SECTION cs;
+
+void modifyArray(LPVOID) {
+    srand(time(0));
+
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        arr[i] = rand() % 301 - 150;
+    }
+}
+
+void squareNegativeNumbers(LPVOID) {
+    while (true) {
+        EnterCriticalSection(&cs);
+        for (int i = 0; i < ARRAY_SIZE; ++i) {
+            if (arr[i] < 0) {
+                arr[i] *= arr[i];
+            }
+        }
+        LeaveCriticalSection(&cs);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
+void printArray(LPVOID) {
+    while (true) {
+        EnterCriticalSection(&cs);
+        for (int i = 0; i < ARRAY_SIZE; ++i) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+        LeaveCriticalSection(&cs);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
+int main() {
+    InitializeCriticalSection(&cs);
+    modifyArray(nullptr);
+
+    HANDLE hThread1 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)modifyArray, NULL, 0, NULL);
+    HANDLE hThread2 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)squareNegativeNumbers, NULL, 0, NULL);
+    HANDLE hThread3 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)printArray, NULL, 0, NULL);
+
+    WaitForSingleObject(hThread1, INFINITE);
+    WaitForSingleObject(hThread2, INFINITE);
+    WaitForSingleObject(hThread3, INFINITE);
+
+    DeleteCriticalSection(&cs);
+    CloseHandle(hThread1);
+    CloseHandle(hThread2);
+    CloseHandle(hThread3);
+
+    return 0;
+}
